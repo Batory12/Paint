e2f15a63fd41167c1260e8a3dc232737d16d250c
Batory12 <bartoldo2004@gmail.com>
2024-04-30 21:30:56 +0200

    funkcja 1

--- src/Plansza.java
+++ src/Plansza.java
@@ -1,15 +1,16 @@
 import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
 
 public class Plansza extends Pane {
     public Plansza(Rysuj r) {
-        setPrefSize(600,600);
-        setOnDragDetected(e -> {
-            getChildren().add(r.rysuj(e.getX(), e.getY(), 100));
+        setMaxSize(800, 600);
+        setOnMouseClicked(e -> {
+            System.out.println("Kliknieto plansze");
+            r.ustawPozycje(e.getX(), e.getY());
+            r.rysuj(this);
         });
-        setOnMouseDragged(e -> {
-        });
-        
-    }
+    };
 }
--- src/Rysuj.java
+++ src/Rysuj.java
@@ -1,20 +1,15 @@
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 public class Rysuj {
     private Figury tryb;
-    private boolean czyWypelniony;
     private boolean czyAktywny=false;
     private double x, y;
     public void ustawTryb(Figury tryb) {
         this.tryb = tryb;
-    }
-    public void ustawPoz(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-    public void ustawWypelnienie(boolean czyWypelniony) {
-        this.czyWypelniony = czyWypelniony;
     }
     public boolean stan() {
         return czyAktywny;
@@ -22,13 +17,47 @@ public class Rysuj {
     public void zmienStan() {
         czyAktywny = !czyAktywny;
     }
-    public Shape rysuj(double x, double y, double r) {
+    public void ustawPozycje(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    private Circle rysujKolo() {
+        System.out.println("Rysuje kolo");
         return new Circle() {
             {
                 setCenterX(x);
                 setCenterY(y);
-                setRadius(r);
+                setRadius(100);
             }
         };
     }
+    private Rectangle rysujProstokat() {
+        System.out.println("Rysuje prostokat");
+        return new Rectangle() {
+            {
+                setX(x);
+                setY(y);
+                setWidth(100);
+                setHeight(100);
+            }
+        };
+    }
+    private Trojkat rysujTrojkat() {
+        System.out.println("Rysuje trojkat");
+        return new Trojkat(x, y, 200);
+    }
+    public void rysuj(Plansza plansza) {
+        switch (tryb) {
+            case KOLO:
+                plansza.getChildren().add(rysujKolo());
+                break;
+            case PROSTOKAT:
+            plansza.getChildren().add(rysujProstokat());
+                break;
+            case TROJKAT:
+            plansza.getChildren().add(rysujTrojkat());
+        }
+    }
+    
+    
 }
